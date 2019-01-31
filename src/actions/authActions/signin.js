edc65830af24a8a869babf65113dc184c7e6a2ca
Michael Ekpang <weezyval93@gmail.com>
2019-01-31 12:34:40 +0100

    ft(signin): user should be able to signin
    - create user signin component
    - update route
    
    [Finishes #162154146]

---
+++ src/actions/authActions/signin.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import * as actionTypes from './actionTypes';
+
+/**
+ * @description - signin start actions
+ * @returns {object} - signin start action
+ */
+export const signinStart = () => ({
+  type: actionTypes.SIGNIN_USER_START
+});
+
+/**
+ * @description - signin success actions
+ * @param {*} payload - success response
+ * @returns {object} - signin sucess action
+ */
+export const signinSuccess = (payload) => ({
+  type: actionTypes.SIGNIN_USER_SUCCESS,
+  payload
+});
+
+/**
+ * @description - signin failure actions
+ * @param {*} payload - failure response
+ * @returns {object} - signin start action
+ */
+export const signinFailure = (payload) => ({
+  type: actionTypes.SIGNIN_USER_FAILURE,
+  payload
+});
+
+/**
+ * @description - signin user
+ * @param {*} user - user object to dispatch
+ * @returns {fn} - signin dispatch function
+ */
+export const signinUser = (user, history) => async (dispatch) => {
+  const data = {
+    ...user
+  };
+  dispatch(signinStart());
+  try {
+    const res = await axios.post(
+      'https://fast-food-api.herokuapp.com/api/v1/auth/login',
+      data
+    );
+    localStorage.setItem('userData', JSON.stringify(res.data[2].rows));
+    localStorage.setItem('fastFoodToken', res.data[1].token);
+    console.log(res.data);
+    dispatch(signinSuccess(res.data));
+    history.push('/');
+  } catch (err) {
+    dispatch(signinFailure(err));
+  }
+};
