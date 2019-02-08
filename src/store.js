fd609b372226da1f0f50fa95f1e9dbcce5a59db5
Michael Ekpang <weezyval93@gmail.com>
2019-02-08 09:57:05 +0100

    fix(compose): devtools-compose
    - update store file
    
    [Finishes]

--- src/store.js
+++ src/store.js
@@ -3,15 +3,13 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
-const middleware = [thunk];
+const devTools = [
+  applyMiddleware(thunk),
+  ...(window.__REDUX_DEVTOOLS_EXTENSION__
+    ? [window.__REDUX_DEVTOOLS_EXTENSION__()]
+    : [])
+];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer, initialState, compose(...devTools));
 
 export default store;
